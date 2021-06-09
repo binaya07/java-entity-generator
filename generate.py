727d0409286279506469220e98745b4d2c5ce7ae
Binaya Sharma <binayasharma@lftechnology.com>
2021-06-09 19:58:59 +0545

    Support column names inside inverted commas as generated by sql developer.

--- generate.py
+++ generate.py
@@ -1,19 +1,24 @@
+import re
+
 # Takes class name and columns as input
 class_name = input('Enter class name : ').strip().capitalize()
 columns = input(
-    'Enter columns as comma separated values (eg column1, column2) : ').split(',')
+    'Enter columns as comma separated values (eg "column1", "column2") : ').split(',')
 
 # Create Java file
 file_name = class_name + '.java'
 file = open(file_name, 'w+')
 
+columnNameRegex = re.compile(r'"(.*)"')
+
 # Create Java entity
 java_entity = 'public class ' + class_name + ' {\n\n'
 for column in columns:
-    column = column.strip()
+    column = columnNameRegex.search(column).group(1).strip()
     if column != '':
         java_entity += '@Column(name="' + column + '")\n'
-        java_entity += 'private String ' + column.replace('_', '') + ';\n\n'
+        variable_name = column.replace('_', '').replace(' ', '').lower()
+        java_entity += 'private String ' + variable_name + ';\n\n'
 
 java_entity += '}'
 
